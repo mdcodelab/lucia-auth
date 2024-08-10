d16012d020948a58ed649737e23c0d607f8e84bc
miha11-hash <d_mihaela@msn.com>
2024-08-10 17:03:53 +0300

    prisma setup

--- app/auth/register/page.js
+++ app/auth/register/page.js
@@ -3,68 +3,57 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Label } from "@radix-ui/react-label";
+import toast from "react-hot-toast";
+import { useState } from "react";
 
 function RegisterPage() {
+  
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repassword, setRePassword] = useState("");
+
+  const handleSubmit = async (e)=> {
+    e.preventDefault();
+  }
+
   return (
     <div className="w-full items-center justify-center">
       <h1 className="text-center text-3xl font-semibold py-6">Welcome</h1>
-      <form className="max-w-[350px] w-full shadow rounded-xl mx-auto">
+      <form className="max-w-[350px] w-full shadow rounded-xl mx-auto" onSubmit={handleSubmit}>
 
         <div className="w-75 mb-2">
-          <Label htmlFor="name" className="text-xl font-semibold">
-            Name:
-          </Label>
-          <input
-            type="text"
-            name="name"
-            className="w-full py-2 shadow rounded-md bordered
+          <Label htmlFor="name" className="text-xl font-semibold">Name:</Label>
+          <input type="text" name="name" className="w-full py-2 shadow rounded-md bordered
             border-slate-300 border-2"
-            placeholder="Your name ..."
-          ></input>
+            placeholder="Your name ..." required onChange={(e)=>setName(e.target.value)}></input>
         </div>
 
         <div className="w-75 mb-2">
-          <Label htmlFor="email" className="text-xl font-semibold">
-            Email:
-          </Label>
-          <input
-            type="email"
-            name="email"
+          <Label htmlFor="email" className="text-xl font-semibold">Email:</Label>
+          <input type="email" name="email"
             className="w-full py-2 shadow rounded-md bordered
             border-slate-300 border-2"
-            placeholder="Your email ..."
-          ></input>
+            placeholder="Your email ..." required onChange={(e)=>setEmail(e.target.value)}></input>
         </div>
 
         <div className="w-75 mb-2">
-          <Label htmlFor="password" className="text-xl font-semibold">
-            Password:
-          </Label>
-          <input
-            type="password"
-            name="password"
+          <Label htmlFor="password" className="text-xl font-semibold">Password:</Label>
+          <input type="password" name="password"
             className="w-full py-2 shadow rounded-md bordered
             border-slate-300 border-2"
-            placeholder="Your password ..."
-          ></input>
+            placeholder="Your password ..." required onChange={(e)=>setPassword(e.target.value)}></input>
         </div>
 
         <div className="w-75 mb-2">
-          <Label htmlFor="rePassword" className="text-xl font-semibold">
-            Retype password:
-          </Label>
-          <input
-            type="password"
-            name="rePassword"
+          <Label htmlFor="rePassword" className="text-xl font-semibold">Retype password:</Label>
+          <input type="password" name="rePassword"
             className="w-full py-2 shadow rounded-md bordered
             border-slate-300 border-2"
-            placeholder="Retype password ..."
-          ></input>
+            placeholder="Retype password ..." required onChange={(e)=>setRePassword(e.target.value)}></input>
         </div>
 
-        <Button type="submit" className="w-full py-2 my-2">
-          Create an account
-        </Button>
+        <Button type="submit" className="w-full py-2 my-2">Create an account</Button>
       </form>
       <div
         className="flex mx-auto justify-between bordered border-black mt-2"
